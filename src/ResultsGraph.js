c27ac73daf33090fb3e77853e159c22ced61e147
sashamaryl <sashamaryl@gmail.com>
2017-12-10 15:26:20 -0500

    save

--- src/ResultsGraph.js
+++ src/ResultsGraph.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import React from 'react';
-import { Line } from 'react-chartjs-2';
+import { Line, Bar } from 'react-chartjs-2';
+import Chart from 'chart.js';
 
 // Logic
 import { getSNAPBenefits } from './programs/federal/snap';
@@ -39,115 +40,128 @@ const ResultsGraph = (props) => {
 
   // TAFDC color? "rgba(206, 125, 61, 1)"
 
-  var data = {
-    labels: xRange,
-    datasets: [
-      {
-        label: "SNAP",
-        borderColor: "rgba(101, 47, 138, 1)",
-        data: snapData,
-        fill: false
-      },
-      {
-        label: "Section 8 Housing",
-        borderColor: "rgba(206, 203, 61, 1)",
-        data: housingData,
-        fill: false
-      },
-    ]
-  };
 
-  var options = {
-    title: {
-      display: true,
-        text: 'Benefit Eligibility for Household Size ' +
-                props.client.householdSize
+//in: current income, future income, current benefits snap and sec8, future benefits snap and sec8 
+
+
+
+  var myBarChart = {
+    data: {
+      labels: ['now', 'future'],
+      datasets: [
+        {
+          label: ['Income'],
+          data: [fakeClient.current.earned, fakeClient.future.earned],
+          borderColor: 'maroon',
+          backgroundColor: 'red'
+        },
+        {
+          label: ["SNAP"], 
+          data: [1300, 1150],
+          borderColor: 'rgba(40,40,40,1)',
+          backgroundColor: 'rgba(40,40,40, 0.5)'
+        },
+        {
+          label: ["Section-8"],
+          data: [2800,1800],
+          borderColor: 'blue',
+          backgroundColor: 'aqua'
+        }
+      ]
     },
-    showLines: true,
-    scales: {
-        yAxes: [{
-          scaleLabel: {
-            display: true,
-              labelString: 'Benefit Value ($)'
-          },
-          ticks: {
-              beginAtZero: true,
+    options: {
+      responsive: true, 
+      title: {
+        display: true,
+          text: 'Benefit Eligibility for Household Size ' +
+                  props.client.householdSize
+      },
+      showLines: true,
+      stacked: true, 
+      maxBarThickness: 0.8, 
+      categoryPercentage: 0.5, 
+      barPercentage: 0.9, 
+      scales: {
+          yAxes: [{
+            stacked: true, 
+            scaleLabel: {
+              display: true,
+                labelString: 'Annual Income ($)'
+            },
+            ticks: {
+                beginAtZero: true,
+                /*
+                 * function to add $ and 1,000s separators to graph axes
+                 * we are using chart.js v2.7 so it requires a callback function
+                 */
+                callback: function(label) {
+                    return label.toLocaleString("en-US");
+                }
+            }
+          }],
+          xAxes: [{
+            stacked: true
+          }]
+      }
+    },
+        
+    tooltips: {
+          callbacks: {
+              // format the title of the tooltips to be in USD
+              title: function(tooltipItems, data) {
+                  return data.labels[tooltipItems[0].index].toLocaleString("en-US",
+                      {style:"currency",currency:"USD"}).replace('.00','');
+              },
               /*
-               * function to add $ and 1,000s separators to graph axes
-               * we are using chart.js v2.7 so it requires a callback function
+               * to add number formatting to the tooltips. returns the data label
+               * + currency format
+               * from https://github.com/chartjs/Chart.js/issues/2386
                */
-              callback: function(label) {
-                  return label.toLocaleString("en-US");
+              label: function(tooltipItem, data) {
+                  return data.datasets[tooltipItem.datasetIndex].label + ": " +
+                      tooltipItem.yLabel.toLocaleString("en-US",{style:"currency",
+                          currency:"USD"}).replace('.00','');
               }
           }
-        }],
-        xAxes: [{
-          scaleLabel: {
-            display: true,
-              labelString: 'Annual Income ($)'
-          },
-            ticks: {
-              callback: function(label) {
-                  return label.toLocaleString("en-US");
-              }
-            }
-        }]
+      }
+    }; 
+
+  
+
+var myTimeline = {
+  type: 'Line', 
+  data: {
+    datasets: [{
+      label: ['Edge Distance'],
+      data: [fakeClient.future.earned, 400, (fakeClient.future.earned + 300)]
+    }],
+    labels: ["income", "current benefit", "lose benefit"]
+  }
+},
+  options: {
+    scales: {
+      xAxes: {
+        stacked:true
+      }
     },
-      /*
-       * default tooltip for chart.js 2.0+  when unspecified looks like:
-       *
-       * options: {
-       *   tooltips: {
-       *       callbacks: {
-       *           label: function(tooltipItem, data) {
-       *               return tooltipItem.yLabel;
-       *           }
-       *       }
-       *   }
-       * }
-       *
-       */
-    tooltips: {
-        callbacks: {
-            // format the title of the tooltips to be in USD
-            title: function(tooltipItems, data) {
-                return data.labels[tooltipItems[0].index].toLocaleString("en-US",
-                    {style:"currency",currency:"USD"}).replace('.00','');
-            },
-            /*
-             * to add number formatting to the tooltips. returns the data label
-             * + currency format
-             * from https://github.com/chartjs/Chart.js/issues/2386
-             */
-            label: function(tooltipItem, data) {
-                return data.datasets[tooltipItem.datasetIndex].label + ": " +
-                    tooltipItem.yLabel.toLocaleString("en-US",{style:"currency",
-                        currency:"USD"}).replace('.00','');
-            }
-        }
     }
   };
 
-  // return (
-  //   <wrapper className = 'result-page'>
-  //     <FormPartsContainer
-  //       title     = {'Results'}
-  //       left      = {{ name: 'Go Back', func: props.previousStep }}
-  //       right     = {{ name: 'Save Results', func: () => props.saveForm(false) }}
-  //        <div> <Line data={data} options={options} /> </div>
-  //     </FormPartsContainer>
-  //   </wrapper>
-  // )
-
   // Non-saving version for first prototype testing
+  
+
   return (
+    
+  
+
     <wrapper className = 'result-page flex-item flex-column'>
       <FormPartsContainer
         title     = {'Results'}
         left      = {{ name: 'Go Back', func: props.previousStep }}
         right      = {{ name: 'Reset', func: function(){ document.location.reload() } }}
       >
-         <div> <Line data={data} options={options} /> </div>
+         <div> <Bar data={myBarChart.data} options={myBarChart.options} /> </div>
+         <div> <Bar data={myTimeline.data} options={myTimeline.options} /> </div>
       </FormPartsContainer>
     </wrapper>
   )
