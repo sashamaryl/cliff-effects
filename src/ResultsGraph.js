f7a5e1c012630ff69b3fc296e8b1da4a103cf2e3
sashamaryl <sashamaryl@gmail.com>
2017-12-12 20:51:30 -0500

    horizontalBar graphs

--- src/ResultsGraph.js
+++ src/ResultsGraph.js
@@ -2,6 +2,7 @@ import _ from 'lodash'
 import React from 'react';
 import { Line, Bar, HorizontalBar } from 'react-chartjs-2';
 import Chart from 'chart.js';
+import { Divider } from 'semantic-ui-react';
 
 // Logic
 import { getSNAPBenefits } from './programs/federal/snap';
@@ -43,126 +44,181 @@ const ResultsGraph = (props) => {
 
 
 
-  var myBarChart = {
-    data: {
-      labels: ['now', 'future'],
-      datasets: [
-        {
-          label: ['Income'],
-          data: [fakeClient.current.earned, fakeClient.future.earned],
-          borderColor: 'maroon',
-          backgroundColor: 'red'
-        },
-        {
-          label: ["SNAP"], 
-          data: [1300, 1150],
-          borderColor: 'rgba(40,40,40,1)',
-          backgroundColor: 'rgba(40,40,40, 0.5)'
-        },
-        {
-          label: ["Section-8"],
-          data: [2800,1800],
-          borderColor: 'blue',
-          backgroundColor: 'aqua'
-        }
-      ]
-    },
-    options: {
-      responsive: true, 
-      title: {
-        display: true,
-          text: 'Benefit Eligibility for Household Size ' +
-                  props.client.householdSize
-      },
-      showLines: true,
-      stacked: true, 
-      maxBarThickness: 0.8, 
-      categoryPercentage: 0.5, 
-      barPercentage: 0.9, 
-      scales: {
-          yAxes: [{
-            stacked: true, 
-            scaleLabel: {
-              display: true,
-                labelString: 'Annual Income ($)'
-            },
-            ticks: {
-                beginAtZero: true,
-                /*
-                 * function to add $ and 1,000s separators to graph axes
-                 * we are using chart.js v2.7 so it requires a callback function
-                 */
-                callback: function(label) {
-                    return label.toLocaleString("en-US");
-                }
-            }
-          }],
-          xAxes: [{
-            stacked: true
-          }]
-      }
-    },
-        
-    tooltips: {
-          callbacks: {
-              // format the title of the tooltips to be in USD
-              title: function(tooltipItems, data) {
-                  return data.labels[tooltipItems[0].index].toLocaleString("en-US",
-                      {style:"currency",currency:"USD"}).replace('.00','');
-              },
-              /*
-               * to add number formatting to the tooltips. returns the data label
-               * + currency format
-               * from https://github.com/chartjs/Chart.js/issues/2386
-               */
-              label: function(tooltipItem, data) {
-                  return data.datasets[tooltipItem.datasetIndex].label + ": " +
-                      tooltipItem.yLabel.toLocaleString("en-US",{style:"currency",
-                          currency:"USD"}).replace('.00','');
-              }
-          }
-      }
-    }; 
+
 
 var myTimeline = {
-  type: 'Line ', 
   data: {
-    labels: ['income'],
+    type: 'Bar',
+    labels: ['Eligibility'],
     datasets: [
-      {
-        label: 'income',
-        data: [800],
-        backgroundColor: 'red'
+      { 
+        label: ['Eligible'],
+        data: [500], 
+        backgroundColor: ['rgba(120,50,90,1)']
       },
       {
-
-      data: [1200],
-        backgroundColor: 'green'
+        label: ['Potentially Eligible'],
+        data: [200], 
+        backgroundColor: ['rgba(220,40,40,1)']
       },
       {
-        label: 'future income',
-        data: [(1000)],
-        backgroundColor: 'blue'
-      }
-    ]},
-  height: '5em', 
+        label: ['not eligibile'],
+        data: [400], 
+        backgroundColor: ['rgba(100,180,120,0.4)']
+      }]
+    },
   options: {
+    title: {
+      display: true, 
+      text: 'Your Income Compared to Eligibility Requirements'
+    },
+    // legend: { 
+    //   display: false,
+    //   position: 'left'
+    //  },
     maintainAspectRatio: false, 
-    scales: 
-    {
-      yAxes: 
-      [{
-        display: false
+    scales: {
+      xAxes: [{
+        stacked: true,
+        ticks: { 
+          beginAtZero: true, 
+          min: 0
+        },
+        scaleLabel: {
+          display: true, 
+          labelString: 'Income'
+        }
       }],
-      xAxes: 
-      [{
-        type: 'category',
-        labels: ['income'], 
-        stacked:true
+      yAxes: [{
+        // type: 'category'
+        stacked: true,
+        barPercentage: 1,
+        categoryPercentage: 1
       }]
     }
   }
 };
+
+var myTimelineDoubleTime = {
+  data: {
+    type: 'Bar',
+    labels: [ 'Income', 'Eligibility'],
+    datasets: [
+      {
+        label: ['Income'],
+        data: [500,0],
+        backgroundColor: 'teal'
+      },
+      { 
+        label: ['Eligible'],
+        data: [0, 300], 
+        backgroundColor: 'rgba(30, 180, 20, 1)'
+      },
+      {
+        label: ['Potentially Eligible'],
+        data: [0, 100], 
+        backgroundColor: 'rgba(220,40,40,1)'
+      },
+      {
+        label: ['Not Eligibile'],
+        data: [0,50], 
+        backgroundColor: 'rgba(100,180,120,0.5)'
+      }]
+    },
+  options: {
+    title: {
+      display: true, 
+      text: 'Your Income Compared to Eligibility Requirements V2'
+    },
+    // maintainAspectRatio: false, 
+    scales: {
+      xAxes: [
+      {
+        stacked: true,
+        ticks: 
+        { 
+          beginAtZero: true, 
+          min: 0
+        },
+        scaleLabel: 
+        {
+          display: true, 
+          labelString: 'Income'
+        }
+      }],
+
+      yAxes: [
+      {
+        // type: 'category'
+        barPercentage: 1,
+        categoryPercentage: 1,
+        ticks: 
+        {
+          beginAtZero: true
+          // max: 1000
+        },
+        stacked: true
+      }]
+    }
+  }
+};
+
+
+
+// var myBarChart = {
+//   data: {
+//     labels: ['now', 'future'], 
+//     datasets: [
+//       {
+//         label: ['Income'],
+//       data: [fakeClient.current.earned, fakeClient.future.earned], 
+//       backgroundColor: 'rgba(120, 30, 60, 1)'
+//       }
+//     ]
+//   }, 
+//   options: {
+//     responsive: true, 
+//     title: {
+//       display: true, 
+//       text: 'Benefit Eligibility for Household Size'
+//     },
+//     showLines: true, 
+//     stacked: true, 
+//     scales: {
+//       yAxes: {
+//         stacked: true, 
+//         scaleLabel: {
+//           display: true, 
+//           labelString: 'Annual Income ($)'
+//         },
+//         ticks: {
+//           beginAtZero: true
+//         }
+//       }
+//     },
+
+//     tooltips: {
+//           callbacks: {
+//               // format the title of the tooltips to be in USD
+//               title: function(tooltipItems, data) {
+//                   return data.labels[tooltipItems[0].index].toLocaleString("en-US",
+//                       {style:"currency",currency:"USD"}).replace('.00','');
+//               },
+//               /*
+//                * to add number formatting to the tooltips. returns the data label
+//                * + currency format
+//                * from https://github.com/chartjs/Chart.js/issues/2386
+//                */
+//               label: function(tooltipItem, data) {
+//                   return data.datasets[tooltipItem.datasetIndex].label + ": " +
+//                       tooltipItem.yLabel.toLocaleString("en-US",{style:"currency",
+//                           currency:"USD"}).replace('.00','');
+//               }
+//           }
+//       }
+//     }
+//   }; 
+
 
 
 
@@ -179,9 +235,22 @@ var myTimeline = {
         left      = {{ name: 'Go Back', func: props.previousStep }}
         right      = {{ name: 'Reset', func: function(){ document.location.reload() } }}
       >
-         <div> <Bar data={myBarChart.data} options={myBarChart.options} /> </div>
-         <div> <Bar data={myTimeline.data} options={myTimeline.options} /></div>
-      </FormPartsContainer>
+         <div> <HorizontalBar data={myTimeline.data} options={myTimeline.options} padded={'very'} /></div>
+
+         <Divider hidden padded={'very'} />
+         <Divider padded={'very'} />
+         <Divider hidden padded={'very'} />
+
+         <div> <HorizontalBar data={myTimelineDoubleTime.data} options={myTimeline.options} padded={'very'} /></div>
+
+         <Divider hidden padded={'very'} />
+         <Divider padded={'very'} />
+         <Divider hidden padded={'very'} />
+
+{/*         <div> <Bar data={myBarChart.data} options={myBarChart.options} compact={true} /> </div>*/}
+
+
+  </FormPartsContainer>
     </wrapper>
   )
 
