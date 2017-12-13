51686669880ef103e3715f91ecc9cfd7d4fdbf5e
WillMcIntosh <mcintoshwilliamh@gmail.com>
2017-12-12 21:04:45 -0500

    styled Home Page

--- src/utils/style.js
+++ src/utils/style.js
@@ -1,13 +1,43 @@
-
-// for AboutContent
-
-// for AboutPage
+// AboutPage and AboutContent
 const aboutPage = {
     fontSize: '4em',
     padding: '8em 0em',
 };
 
+// Home Page
+const titleBox = {
+    display: 'inline-block',
+};
+const homePageTitle = {
+    fontSize: '5em',
+    color: '#efefef',
+    marginBottom: '-15px',
+};
+const homePageSubtitle = {
+    fontSize: '2em',
+    color: '#efefef',
+    textAlign: 'right',
+    marginTop: '-15px',
+    marginRight: '45px',
+};
+const centerContents = {
+    marginTop: '3em',
+    marginBottom: '3em',
+};
+const homePageButton = {
+    backgroundColor: '#ffffffc8',
+    textColor: '#00b5ad',
+    fontSize: '1.5em',
+    marginRight: '1.5em',
+    marginLeft: '1.5em',
+};
+const prototypeWarning = {
+    color: '#efefef',
+    textAlign: 'center',
+    fontSize: '1em',
+}
 
 export {
-    aboutPage,
+    aboutPage, titleBox, homePageTitle, homePageSubtitle, centerContents,
+    homePageButton, prototypeWarning,
 }
